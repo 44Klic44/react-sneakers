8775a290e422de9068000d33b5db0401d362335d
ROMAN TYSACHNYY <stance_srt@mail.ru>
2025-06-10 17:47:39 +0300

    2 верстка

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ import './App.scss';
 function App() {
   return (
     <div className="wrapper clear">
+
+      <div className='overlay'>
+      <div className='drawer'>
+<h2>Корзина</h2>
+
+
+<div className='cartItem'>
+  <img width={133} height={112} src='https://static.tildacdn.com/stor3830-6632-4431-a664-353039626532/52817993.jpg'/>
+
+   <div>
+    <p>Stylysh nide - 10ml </p>
+    <b> 4000 руб.</b>
+   </div>
+
+<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB"/>
+<path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5"/>
+</svg>
+
+</div>
+
+
+      </div>
+</div>
+
+
    <header className='d-flex justify-between align-center p-40'>
     <div className='d-flex alighn-center headerleft'> 
      <img width={40} height={40} src="https://static.tildacdn.com/tild3336-3038-4338-a664-363562396339/Frame_14.jpg" alt='logo'/>
@@ -28,12 +54,29 @@ function App() {
        </ul>
 
    </header>
-   <div className='content  p-40'>
-<h1 className='mb-40'>Все пигменты</h1>
 
+<div className='content p-40'>
+
+   <div className='d-flex align-center justify-between content mb-40  '>
+<h1 className=''>Все пигменты</h1>
+
+<div className='search-block d-flex align-center'>
+  <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="18px" height="18px"><path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"/></svg>
+<input placeholder='Поиск'></input>
+</div>
+
+  </div>
 <div className='d-flex'>
 
  <div className='card'>
+  
+<div className='favorite'>
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="31" height="31" rx="6.5" fill="white" stroke="#F8F8F8"/>
+<path d="M21.8609 11.0746C21.5204 10.7339 21.1161 10.4636 20.6711 10.2793C20.2261 10.0949 19.7492 10 19.2675 10C18.7859 10 18.3089 10.0949 17.864 10.2793C17.419 10.4636 17.0147 10.7339 16.6742 11.0746L15.9675 11.7812L15.2609 11.0746C14.5731 10.3868 13.6402 10.0004 12.6675 10.0004C11.6948 10.0004 10.762 10.3868 10.0742 11.0746C9.3864 11.7623 9 12.6952 9 13.6679C9 14.6406 9.3864 15.5734 10.0742 16.2612L10.7809 16.9679L15.9675 22.1546L21.1542 16.9679L21.8609 16.2612C22.2015 15.9207 22.4718 15.5164 22.6561 15.0715C22.8405 14.6265 22.9354 14.1495 22.9354 13.6679C22.9354 13.1862 22.8405 12.7093 22.6561 12.2643C22.4718 11.8193 22.2015 11.4151 21.8609 11.0746Z" stroke="#EAEAEA" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</div>
+
 <img width={133} height={112} src='https://static.tildacdn.com/stor3830-6632-4431-a664-353039626532/52817993.jpg'/>
 <h5>Bronza • 10 ml</h5>
 <div className='d-flex justify-between align-center'>
@@ -96,6 +139,7 @@ function App() {
 
    </div>
    
+  
     </div>
   );
 }
