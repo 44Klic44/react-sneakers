879fd2fdb06048d0d29e5b182775fc79815d310a
ROMAN TYSACHNYY <stance_srt@mail.ru>
2025-06-10 20:03:15 +0300

    3 верстка

--- src/App.js
+++ src/App.js
@@ -5,25 +5,69 @@ function App() {
   return (
     <div className="wrapper clear">
 
-      <div className='overlay'>
-      <div className='drawer'>
+      <div className='overlay '>
+      <div className='drawer '>
 <h2>Корзина</h2>
 
 
-<div className='cartItem'>
-  <img width={133} height={112} src='https://static.tildacdn.com/stor3830-6632-4431-a664-353039626532/52817993.jpg'/>
+<div className='items'>
+<div className='cartItem mb-20 d-flex align-center justify-between'>
+  
+<div 
+  className='cartItemImg '
+  style={{ backgroundImage: "url(https://static.tildacdn.com/stor3830-6632-4431-a664-353039626532/52817993.jpg)" }}
+></div>
 
-   <div>
-    <p>Stylysh nide - 10ml </p>
+
+  {/* <img className='mr-20' width={70} height={70} src='https://static.tildacdn.com/stor3830-6632-4431-a664-353039626532/52817993.jpg'/> */}
+
+   <div className='mr-20'>
+    <p className='mb-5'>Stylysh nide - 10ml </p>
     <b> 4000 руб.</b>
    </div>
 
-<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg className='removeBtn' width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB"/>
 <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5"/>
 </svg>
 
 </div>
+
+<div className='cartItem mb-20 d-flex align-center justify-between'>
+  
+<div 
+  className='cartItemImg '
+  style={{ backgroundImage: "url(https://static.tildacdn.com/stor3830-6632-4431-a664-353039626532/52817993.jpg)" }}
+></div>
+
+
+  {/* <img className='mr-20' width={70} height={70} src='https://static.tildacdn.com/stor3830-6632-4431-a664-353039626532/52817993.jpg'/> */}
+
+   <div className='mr-20'>
+    <p className='mb-5'>Stylysh nide - 10ml </p>
+    <b> 4000 руб.</b>
+   </div>
+
+<svg className='removeBtn' width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB"/>
+<path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5"/>
+</svg>
+
+</div>
+
+</div>
+ <ul className='cartTotalBlock'>
+  <li >
+    <span>Итого</span>
+    <div></div>
+    <b>21 500 руб.</b>
+  </li>
+  <li >
+    <span>Налог 5%</span>
+    <div></div>
+    <b>1074 руб.</b>
+  </li>
+</ul>
 
 
       </div>
